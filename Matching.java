eaa69af6eb8e823d6c92f241f9b61e638a6dae43
Shintaro <30695483+shintaroDesu@users.noreply.github.com>
2018-05-29 13:05:53 +0200

    Update Matching.java

--- Matching.java
+++ Matching.java
@@ -1,3 +1,36 @@
 public class Matching
 {
+	public static void main(String[] args)
+	{
+		Solution solution = new Solution();
+		System.out.println(solution.isMatch("mississippi","mis*is*p*."));
+	}
+}
+
+class Solution
+{
+	Solution()
+	{}
+	
+	public boolean isMatch(String s, String p)
+	{
+		String[] pattern = p.split("*");
+		int p_pos = 0;
+		
+		while(s.length() > 0)
+		{
+			comparee = pattern[p_pos];
+			my_sub = s.substring(0,comparee.length());
+			if(comparee.equals(my_sub))
+			{
+				s = s.substring(comparee.length - 1, s.length());
+			}
+			else
+			{
+				p_pos++;
+			}
+		}
+		
+		return true;
+	}
 }
