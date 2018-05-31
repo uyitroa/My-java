7ad8bb41d9eed504ea3b213e7b960370e5bcbbc3
Shintaro <30695483+shintaroDesu@users.noreply.github.com>
2018-05-31 12:49:25 +0200

    Update convertisseur.java

--- convertisseur.java
+++ convertisseur.java
@@ -1,1 +1,38 @@
+public class convertisseur
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
 
+class Convert
+{
+	String[] values;
+	
+	Convert(String[] values)
+	{
+		this.values = values;
+	}
+	
+	public String[] get()
+	{
+		String my_string;
+		String[] shit, result;
+		float value;
+
+		for(int x = 0; x < values.length; x++)
+		{
+			my_string = values[x];
+			shit = my_string.split(" ");
+			value = Float.parseFloat(shit[0]);
+			char unit = my_string.charAt(my_string.length() - 1);
+			
+			if(unit == 'g')
+			{
+				
+			}
+		}
+	}
+
+}
